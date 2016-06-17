362313b18241b7d561341389bbb1b19b04591564
Thomas Torfs <thomas.torfs@gmail.com>
2016-06-17 19:18:26 +0200

    Now supporting rendering template data

--- index.js
+++ index.js
@@ -9,68 +9,69 @@ const mkdirp = require('mkdirp');
 
 class StaticMarkoCompiler {
   constructor(brunchConfig) {
-    // Get the Brunch configuration for marko
+    // Get the Brunch configuration for marko.
     this.config = brunchConfig && brunchConfig.plugins && brunchConfig.plugins.marko || {};
 
-    // Complete the config with defaults where necessary
+    // Complete the config with defaults where necessary.
     this.config = _.defaults(this.config, {
-      'createJs': true,
-      'templatePath': 'app/views/templates',
-      'jsTemplatePath': 'app/views/templates/subdir'
+      createJs: true,
+      templatePath: 'app/views',
+      jsTemplatePath: 'app/views/content',
     });
 
-    // Also store the public path for later usage
+    // Get the data.
+    this.templateData = brunchConfig.templateData || {};
+
+    // Also store the public path for later usage.
     this.publicPath = brunchConfig.paths.public;
 
-    // Make sure marko doesn't write any files by itself
-    require('marko/compiler').defaultOptions.writeToDisk = false;
-
-    // Enable hot reload
+    // Enable hot reload.
     require('marko/hot-reload').enable();
   }
 
   compile(params) {
     return new Promise((resolve, reject) => {
-      let result, error;
+      let config, publicPath, result, staticResult, error;
+
+      // Store the config in a local variable so functions can use it.
+      config = this.config;
+      publicPath = this.publicPath;
 
       try {
-        let template, staticResult;
-
-        // Let marko handle the modified file
+        // Let marko handle the modified file.
         require('marko/hot-reload').handleFileModified(params.path);
 
-        // Create the static file
-        if (params.path.indexOf(this.config.templatePath) === 0) {
-          // Define the output path
-          var outputPath = path.join(
-            this.publicPath,
-            path.relative(this.config.templatePath, path.dirname(params.path)),
-            path.basename(params.path, '.marko') + '.html'
-          );
-
-          // Create it if it doesn't exist yet
-          mkdirp(path.dirname(outputPath));
-
-          // Render the template
-          template = marko.load(params.path);
-          staticResult = template.renderSync();
-
-          // Write the result to a file
-          fs.writeFile(outputPath, staticResult);
-        }
-
-        // Create the JS result
-        // Since everything is static, we do not use the built-in Marko
-        // compiler in order to generate CommonJS modules, at least for now.
-        if (this.config.createJs && params.path.indexOf(this.config.jsTemplatePath) === 0) {
-          if (this.config.jsTemplatePath !== this.config.templatePath) {
-            // Render the template
-            template = marko.load(params.path);
-            staticResult = template.renderSync();
+        // Render the template.
+        let template;
+        template = marko.load(params.path, {writeToDisk: false});
+        template.render(this.templateData, function(err, html, out) {
+          if (err) {
+            console.error('An error occurred: ' + err);
+            return;
           }
-          staticResult = JSON.stringify(staticResult);
-          result = umd(staticResult);
-        }
+
+          // Create the JS result.
+          // Since everything is static, we do not use the built-in Marko
+          // compiler in order to generate CommonJS modules, at least for now.
+          if (config.createJs && params.path.indexOf(config.jsTemplatePath) === 0) {
+            result = umd(JSON.stringify(html));
+          }
+          // Create the static file.
+          else if (params.path.indexOf(config.templatePath) === 0) {
+            // Define the output path.
+            let outputPath = path.join(
+              publicPath,
+              path.relative(config.templatePath, path.dirname(params.path)),
+              path.basename(params.path, '.marko') + '.html'
+            );
+
+            // Create the output directory if it doesn't exist yet.
+            mkdirp(path.dirname(outputPath));
+
+            // Write the resulting html to the destination
+            fs.writeFile(outputPath, html);
+          }
+        });
       }
       catch (_error) {
         error = _error;
@@ -82,7 +83,6 @@ class StaticMarkoCompiler {
         resolve(result);
       }
     });
-
   }
 }
 
@@ -91,7 +91,3 @@ StaticMarkoCompiler.prototype.type = 'template';
 StaticMarkoCompiler.prototype.extension = 'marko';
 
 module.exports = StaticMarkoCompiler;
-
-
-
-
